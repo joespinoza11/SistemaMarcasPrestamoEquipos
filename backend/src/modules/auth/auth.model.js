d40ac54a504283be914406b1782a9c8edb7fddc8
Samus1906s <sacerdasce@est.utn.ac.cr>
2026-08-20 13:05:22 -0600

    Archivos db.js y auth.model.js
    configuración inicial de la base de datos y model de auth, en donde llevará parte de lo que se va a almacenar en la base de datos. Basandome en la busqueda de usuario, departamento, rol,también en la creación de un usuario, en buscar el usuario en el login, recuperación del usuario. Y por último un token de recupración, buscar token de recuepración en este va a verificar si se esta usando no, actulizar la contraseña y marcar que el token se esta usando.

---
+++ backend/src/modules/auth/auth.model.js
@@ -0,0 +1,182 @@
+import pool from "../../config/db";
+
+export async function buscarUsuarioPorCorreo(correo) {
+
+    const [result] = await pool.execute(
+    `SELECT id, 
+    correo
+    FROM usuarios
+    WHERE correo = ?`,
+    [correo]
+    );
+
+    return result[0]
+}
+
+export async function buscarUsuarioPorUsername(username) {
+    
+    const  [ressult] = await pool.execute(
+        `SELECT id,
+        username
+        FROM usuarios
+        WHERE username = ?`,
+        [username]
+    );
+
+    return result[0];
+}
+
+export async function buscarDepartamentoPorId(id) {
+
+    const [result] = await pool.execute(
+        `SELECT id,
+        FROM departamentos
+        WHERE id = ?`,
+        [departamentoId]
+    );
+
+    return result[0];
+} 
+
+export async function buscarRolUsuario() {
+
+    const [result] = await pool. execute(
+        `SELECT id
+        FROM roles
+        WHERE noombre = 'usuario'`
+    );
+
+    return result[0]
+}
+
+export async function crearUsuario(
+    nombreCompleto,
+    fechaNacimiento,
+    correo,
+    username,
+    passwordHash,
+    departamentoId,
+    rolId
+) {
+    const [result] = await pool.execute (
+        `INSERT INTO usuarios(
+        nombre_completo,
+        fecha_nacimiento,
+        correo,
+        username,
+        password_hash,
+        departamento_id,
+        rol_id
+        )
+        
+        VALUES (?,?,?,?,?,?,?)`,
+        [
+            nombreCompleto,
+            fechaNacimiento,
+            correo,
+            username,
+            passwordHash,
+            departamentoId,
+            rolId
+        ]
+    );
+
+    return result.insertId;
+}
+
+export async function buscarUsuarioLogin(usuario) {
+
+    const [result] = await pool.execute(
+        `SELECT
+        u.id,
+        u.nombre_completo,
+        u.fecha_nacimiento,
+        u.correo,
+        u.username,
+        u.password_hash,
+        r.nombre AS rol
+        
+        FROM usuarios u
+        INNER JOIN roles r
+        ON u.rol_id = r.id
+        WHERE u.correo = ?`,
+        [usuario, usuario]
+    );
+
+    return result[0];
+}
+
+export async function buscarUsuarioRecuperacion(usuario){
+
+    const [result] = await pool.execute(
+        `SELECT id,correo,username
+        FROM usuarios
+        WHERE username = ? OR correo = ?`,
+        [usuario, usuario]
+    );
+
+    return result[0];
+}
+
+export async function TokenRecuperacion(usuarioId,token,fechaExpiracion) {
+
+  const [result] = await pool.execute(
+    `INSERT INTO tokens_recuperacion
+      (
+        usuario_id,
+        token,
+        fecha_expiracion
+      )
+     VALUES (?, ?, ?)`,
+    [
+      usuarioId,
+      token,
+      fechaExpiracion
+    ]
+  );
+
+  return result.insertId;
+}
+
+export async function buscarTokenRecuperacion(token) {
+
+    const [result] = await pool.execute(
+        `SELECT id, usuario_id, fecha_expiracion, usado
+        FROM tokens_recuperacion
+        WHERE token = ?
+        AND usado = FALSE
+        AND fecha_expiracion > NOW()`,
+        [token]
+    );
+
+    return result[0];
+}
+
+export async function actualizarPassword(
+  usuarioId,
+  passwordHash
+) {
+
+  const [result] = await pool.execute(
+    `UPDATE usuarios
+     SET password_hash = ?
+     WHERE id = ?`,
+    [
+      passwordHash,
+      usuarioId
+    ]
+  );
+
+  return result.affectedRows;
+}
+
+
+export async function marcarTokenComoUsado(tokenId) {
+
+  await pool.execute(
+    `UPDATE tokens_recuperacion
+     SET usado = TRUE
+     WHERE id = ?`,
+    [tokenId]
+  );
+}
