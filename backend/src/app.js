f7d4284f6825b442f7a769dc04f4a9ba496d2536
Samus1906s <sacerdasce@est.utn.ac.cr>
2026-08-21 15:26:53 -0600

    Archivo app.js
    Se hizo el archivo app.js, en el cual ya esta adaptado para poder levantar el backend o el servidor.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,63 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import session from "express-session";
+
+import authRoutes from "./modules/auth/auth.routes.js";
 
 dotenv.config();
 
+const NAME = process.env.SERVER_NAME;
+const PORT = process.env.SERVER_PORT || 3000;
+
 const app = express();
+
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+  })
+);
+
+
 app.use(express.json());
 
-const PORT = process.env.PORT || 4000;
+
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+
+    cookie: {
+      httpOnly: true,
+      sameSite: "lax",
+      secure: false,
+      maxAge: 60 * 60 * 1000
+    }
+  })
+);
+
+
+
+app.get("/", (req, res) => {
+
+  res.json({
+    nombre: NAME,
+    estado: "Servidor funcionando correctamente"
+  });
+});
+
+
+
+app.use("/api/auth",authRoutes);
+
+
 app.listen(PORT, () => {
-  console.log(`Servidor backend corriendo en el puerto ${PORT}`);
+
+  console.log(
+    `${NAME} ejecutándose en http://localhost:${PORT}`
+  );
 });
